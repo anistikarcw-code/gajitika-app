b9910ac7e31b038853de18ca8454a49fa22cd03a
Anistika Rcw <anistika.rcw@gmail.com>
2025-07-31 17:15:41 +0000

    error

--- public/sw.js
+++ public/sw.js
@@ -1,51 +1,24 @@
-
-const CACHE_NAME = 'gajitika-cache-v1';
-const urlsToCache = [
-  '/',
-  '/manifest.json',
-  '/_offline'
-  // Add other important URLs and assets here
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).catch(() => {
-          // If the network fails, and it's a navigation request,
-          // return the offline page.
-          if (event.request.mode === 'navigate') {
-            return caches.match('/_offline');
-          }
-        });
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles/main.css',
+        '/scripts/main.js',
+        // Add other assets you want to cache
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
+        '/_offline'
+      ]);
     })
   );
 });
