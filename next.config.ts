41c73bc3a77628aba1893b6c83f033cce6a91469
Anistika Rcw <anistika.rcw@gmail.com>
2025-07-31 15:48:33 +0000

    perbaiki

--- next.config.ts
+++ next.config.ts
@@ -23,13 +23,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-    };
-    return config;
-  },
 };
 
 export default withPWA({
