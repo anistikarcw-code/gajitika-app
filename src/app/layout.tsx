9e0c4431e78c2ce135141f47873022f57b2a730e
Anistika Rcw <anistika.rcw@gmail.com>
2025-07-31 17:12:14 +0000

    sudah jadi apk namun tidak dapat di install.paket tidak valid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,21 @@ export default function RootLayout({
           rel="stylesheet"
         ></link>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if ('serviceWorker' in navigator) {
+                window.addEventListener('load', () => {
+                  navigator.serviceWorker.register('/sw.js').then(registration => {
+                    console.log('Service Worker registered with scope:', registration.scope);
+                  }).catch(error => {
+                    console.log('Service Worker registration failed:', error);
+                  });
+                });
+              }
+            `,
+          }}
+        />
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
